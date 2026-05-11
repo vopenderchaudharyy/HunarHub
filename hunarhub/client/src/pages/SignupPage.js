4edee78cb2bed438bdaf881a3607a4f71ef30339
vopenderchaudharyy <vopendechaudhary@gmail.com>
2026-05-11 13:02:51 +0530

    first commit

--- hunarhub/client/src/pages/SignupPage.js
+++ hunarhub/client/src/pages/SignupPage.js
@@ -27,7 +27,7 @@ const SignupPage = () => {
     }
     setLoading(true);
     try {
-      const res = await fetch('http://localhost:5000/api/auth/register', {
+      const res = await fetch(`${process.env.REACT_APP_API_URL}/auth/register`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form),
